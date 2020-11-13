0c8612134ef5de277285bb3e5414b119df4e8f74
AlyciaNSaris <aly2017807@maricopa.edu>
2020-11-13 11:56:26 -0700

    Getting Started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Alycia Saris on 11/13/20.
+//  Copyright © 2020 Alycia Saris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
